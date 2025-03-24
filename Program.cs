d079cc9e05e5915ac86e7d10bbf2a587ffeed30a
ITHanan <155847905+ITHanan@users.noreply.github.com>
2025-03-24 13:33:33 +0100

    I added EntityFramwork and connect to database

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using InventoryManagementWithExpirationDatesSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace InventoryManagementWithExpirationDatesSystem
 {
     public class Program
@@ -13,6 +16,8 @@ namespace InventoryManagementWithExpirationDatesSystem
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<WarehouseManagementSystemContext>(options => options.UseSqlServer("Server=HANAN\\SQLEXPRESS;Database=WarehouseManagementSystem;Trusted_Connection=True;TrustServerCertificate=True;"));
 
             var app = builder.Build();
 
