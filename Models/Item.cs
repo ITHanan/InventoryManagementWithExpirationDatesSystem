8d3d06094aa099abc4f1f23aacb7608d846d3700
ITHanan <155847905+ITHanan@users.noreply.github.com>
2025-03-27 14:45:20 +0100

    i implement populating user in my database using Bogus, fixed DTO

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace InventoryManagementWithExpirationDatesSystem.Models;
+﻿namespace InventoryManagementWithExpirationDatesSystem.Models;
 
 public partial class Item
 {
